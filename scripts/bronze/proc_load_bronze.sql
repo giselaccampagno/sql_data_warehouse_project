84ee29c2dc663ea2eca837e8e7f0a4860f371e23
Gisela Campagno <giselacampagno@gmail.com>
2025-02-13 12:05:01 -0300

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -1,1 +1,50 @@
+/* LIMPIAR LOS CSV. DA ERROR POR DATOS FALTANTES */
+
+LOAD DATA INFILE 
+"C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/source_crm/cust_info.csv"
+INTO TABLE crm_cust_info
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE 
+"C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/source_crm/prd_info.csv"
+INTO TABLE crm_prd_info
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE 
+"C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/source_crm/sales_details.csv"
+INTO TABLE crm_sales_details
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE 
+"C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/source_erp/CUST_AZ12.csv"
+INTO TABLE erp_cust_az12
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE 
+"C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/source_erp/LOC_A101.csv"
+INTO TABLE erp_loc_a101
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+LOAD DATA INFILE 
+"C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/source_erp/PX_CAT_G1V2.csv"
+INTO TABLE erp_px_cat_g1v2
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"' 
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
 
