ced623d37e7982cb39942291d494e3f5667609d3
Gisela Campagno <giselacampagno@gmail.com>
2025-02-16 20:00:17 -0300

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,17 +1,8 @@
-/*
-===============================================================================
-DDL Script: Create Bronze Tables
-===============================================================================
-Script Purpose:
-    This script creates tables in the 'bronze' schema, dropping existing tables 
-    if they already exist.
-	  Run this script to re-define the DDL structure of 'bronze' Tables
-===============================================================================
-*/
+DROP DATABASE IF EXISTS bronze;
+
+CREATE DATABASE bronze;
 
 USE bronze;
-
-DROP TABLE IF EXISTS crm_cust_info;    
 
 CREATE TABLE crm_cust_info (
 		cst_id INT,
@@ -23,8 +14,6 @@ CREATE TABLE crm_cust_info (
         cst_create_date DATE
 );
 
-DROP TABLE IF EXISTS crm_prd_info;    
-
 CREATE TABLE crm_prd_info (
 		prd_id INT,
         prd_key VARCHAR (50),
@@ -34,8 +23,6 @@ CREATE TABLE crm_prd_info (
         prd_start_dt DATETIME,
         prd_end_dt DATETIME
 );
-
-DROP TABLE IF EXISTS crm_sales_details;    
 
 CREATE TABLE crm_sales_details (
 		sls_ord_num VARCHAR (50),
@@ -49,22 +36,16 @@ CREATE TABLE crm_sales_details (
         sls_price INT
 );
 
-DROP TABLE IF EXISTS erp_cust_az12;    
-
 CREATE TABLE erp_cust_az12 (
         cid VARCHAR (50),
         bdate DATE,
         gen VARCHAR (50)
 );
 
-DROP TABLE IF EXISTS erp_loc_a101;    
-
 CREATE TABLE erp_loc_a101 (
         cid VARCHAR (50),
         cntry VARCHAR (50)
 );
-
-DROP TABLE IF EXISTS erp_px_cat_g1v2;    
 
 CREATE TABLE erp_px_cat_g1v2 (
 		id VARCHAR (50),
