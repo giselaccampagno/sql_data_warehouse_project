6b5166dfabfb456970c14c4cf501949384fe4cf8
Gisela Campagno <giselacampagno@gmail.com>
2025-02-17 11:52:11 -0300

    Update loading_silver.sql

--- scripts/silver/loading_silver.sql
+++ scripts/silver/loading_silver.sql
@@ -25,3 +25,24 @@ FROM (
 	FROM bronze.crm_cust_info
     WHERE cst_id IS NOT NULL) AS t
     WHERE flag_last=1; -- Select the most recent record per cuatomer 
+
+-- Loading crm_prd_info Table
+
+INSERT INTO silver.crm_prd_info (
+prd_id, cat_id, prd_key, prd_nm, prd_cost, prd_line, prd_start_dt, prd_end_dt)
+SELECT  
+	prd_id,
+    REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,  
+    SUBSTRING(prd_key, 7, LENGTH(prd_key)) AS prd_key,  
+    TRIM(prd_nm) AS prd_nm,  
+    prd_cost,
+    CASE UPPER(TRIM(prd_line))  
+        WHEN 'S' THEN 'Sport'  
+        WHEN 'M' THEN 'Mountain'  
+        WHEN 'T' THEN 'Touring' 
+        WHEN 'R' THEN 'Road'
+        ELSE 'N/A'  
+    END AS prd_line,
+    prd_start_dt,
+    LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt - 1) prd_end_dt
+FROM bronze.crm_prd_info;
