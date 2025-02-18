627c9170ca96709a4f3ccab06a58f948736f3864
Gisela Campagno <giselacampagno@gmail.com>
2025-02-18 19:25:58 -0300

    Update loading_silver.sql

--- scripts/silver/loading_silver.sql
+++ scripts/silver/loading_silver.sql
@@ -97,3 +97,22 @@ INSERT INTO silver.erp_cust_az12 (
 			ELSE 'N/A'
 		END AS gen
 	FROM bronze.erp_cust_az12;
+
+-- Loading silver.erp_loc_a101 Table
+
+INSERT INTO silver.erp_loc_a101 (
+			cid,
+			cntry
+		)
+		SELECT
+			REPLACE(cid, '-', '') AS cid, 
+			CASE
+				WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+				WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+				WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+				ELSE TRIM(cntry)
+			END AS cntry -- Normalize and Handle missing or blank country codes
+		FROM bronze.erp_loc_a101;
+
+-- Loading silver.px_cat_g1v2 Table
+
