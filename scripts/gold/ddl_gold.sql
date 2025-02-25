5b391c87169c9f0b765149822e8f94cfee0e4147
Gisela Campagno <giselacampagno@gmail.com>
2025-02-25 19:46:24 -0300

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -51,23 +51,24 @@ WHERE pi.prd_end_dt IS NULL -- Filter out all historical data
 );
 
 -- =============================================================================
--- Create Fact Table: gold.fact_sales
+-- Create Fact Table: gold2.fact_sales
 -- =============================================================================
-DROP VIEW IF EXISTS gold.fact_sales;
+DROP VIEW IF EXISTS gold2.fact_sales;
 
-CREATE VIEW gold.fact_sales AS
-SELECT
-    sd.sls_ord_num  AS order_number,
-    pr.product_key  AS product_key,
-    cu.customer_key AS customer_key,
+CREATE VIEW fact_sales AS (
+SELECT 
+	sd.sls_ord_num AS order_number,
+    dp.product_key AS product_key,
+    dc.customer_key AS customer_key,
     sd.sls_order_dt AS order_date,
-    sd.sls_ship_dt  AS shipping_date,
-    sd.sls_due_dt   AS due_date,
-    sd.sls_sales    AS sales_amount,
+    sd.sls_ship_dt AS ship_date,
+    sd.sls_due_dt AS due_date,
+    sd.sls_sales AS sales,
     sd.sls_quantity AS quantity,
-    sd.sls_price    AS price
-FROM silver.crm_sales_details sd
-LEFT JOIN gold.dim_products pr
-    ON sd.sls_prd_key = pr.product_number
-LEFT JOIN gold.dim_customers cu
-    ON sd.sls_cust_id = cu.customer_id;
+    sd.sls_price AS price
+FROM silver2.crm_sales_details AS sd
+LEFT JOIN gold2.dim_customers AS dc
+ON sd.sls_cust_id = dc.customer_id
+LEFT JOIN gold2.dim_products AS dp
+ON sd.sls_prd_key = dp.product_number
+);
