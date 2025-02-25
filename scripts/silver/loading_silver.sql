bbb81080be28f4b6ff1d63261dbd30d9abe6b257
Gisela Campagno <giselacampagno@gmail.com>
2025-02-25 09:57:26 -0300

    Update loading_silver.sql

--- scripts/silver/loading_silver.sql
+++ scripts/silver/loading_silver.sql
@@ -3,31 +3,30 @@
 TRUNCATE TABLE crm_cust_info;
 
 SET @start_time = NOW();
-INSERT INTO silver.crm_cust_info (
-cst_id, cst_key, cst_firstname, cst_lastname, 
-cst_marital_status, cst_gndr, cst_create_date)
+INSERT INTO silver2.crm_cust_info
+(cst_id, cst_key, cst_firstname, cst_lastname, cst_marital_status, cst_gndr, cst_create_date)
 SELECT 
-cst_id,
-cst_key,
-TRIM(cst_firstname) AS cst_firstname,
-TRIM(cst_lastname) AS cst_lastname,
-CASE 
-	WHEN UPPER(TRIM(cst_marital_status))='M' THEN 'Married'
-	WHEN UPPER(TRIM(cst_marital_status))='S' THEN 'Single'
-END AS cst_marital_status, -- Normalize marital status to readable format
-CASE 
-	WHEN UPPER(TRIM(cst_gndr))='F' THEN 'Female'
-	WHEN UPPER(TRIM(cst_gndr))='M' THEN 'Male'
-    ELSE 'N/A'
-END AS cst_gndr, -- Normalize gender values to readable format
-cst_create_date 
-FROM (
-	SELECT 
-    *,
-    ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
-	FROM bronze.crm_cust_info
-    WHERE cst_id IS NOT NULL) AS t
-    WHERE flag_last=1; -- Select the most recent record per cuatomer 
+	cst_id, 
+    cst_key, 
+    TRIM(cst_firstname) AS cst_firstname, 
+    TRIM(cst_lastname) AS cst_lastname, 
+	CASE  
+		WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+        WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+		ELSE 'n/a'
+	END AS cst_marital_status,     -- Normalize marital status values to readable format
+    CASE  
+		WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+        WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+		ELSE 'n/a'
+	END AS cst_gndr,     -- Normalize gender values to readable format
+    cst_create_date
+FROM
+(SELECT 
+	*,
+	ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+FROM bronze2.crm_cust_info) AS t
+WHERE t.flag_last = 1; -- Select more recent record per customer
 SET @end_time = NOW();
 
 SELECT CONCAT(
