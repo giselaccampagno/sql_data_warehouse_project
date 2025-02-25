6e6b8609055e54a7487e3c4c186de125d6fae98d
Gisela Campagno <giselacampagno@gmail.com>
2025-02-25 19:02:26 -0300

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -1,28 +1,29 @@
 -- =============================================================================
--- Create Dimension: gold.dim_customers
+-- Create Dimension: gold2.dim_customers
 -- =============================================================================
-DROP VIEW IF EXISTS gold.dim_customers;
+DROP VIEW IF EXISTS gold2.dim_customers;
 
-CREATE VIEW gold.dim_customers AS
-SELECT
-    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate key
-    ci.cst_id                          AS customer_id,
-    ci.cst_key                         AS customer_number,
-    ci.cst_firstname                   AS first_name,
-    ci.cst_lastname                    AS last_name,
-    la.cntry                           AS country,
-    ci.cst_marital_status              AS marital_status,
+CREATE VIEW gold2.dim_customers AS (
+SELECT 
+	ROW_NUMBER() OVER(ORDER BY ci.cst_id) AS customer_key,
+	ci.cst_id AS customer_id,
+    ci.cst_key AS customer_number,
+    ci.cst_firstname AS fisrt_name,
+    ci.cst_lastname AS last_name,
+    la.cntry AS country,
+    ci.cst_marital_status AS marital_status,
     CASE 
-        WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the primary source for gender
-        ELSE COALESCE(ca.gen, 'n/a')  			   -- Fallback to ERP data
-    END                                AS gender,
-    ca.bdate                           AS birthdate,
-    ci.cst_create_date                 AS create_date
-FROM silver.crm_cust_info ci
-LEFT JOIN silver.erp_cust_az12 ca
-    ON ci.cst_key = ca.cid
-LEFT JOIN silver.erp_loc_a101 la
-    ON ci.cst_key = la.cid;
+		WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
+        ELSE COALESCE(ca.gen, 'n/a')
+	END AS gender,
+    ca.bdate AS birthdate,
+    ci.cst_create_date AS create_date
+FROM silver2.crm_cust_info AS ci
+LEFT JOIN silver2.erp_cust_az12 AS ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver2.erp_loc_a101 AS la
+ON ci.cst_key = la.cid
+);
 
 -- =============================================================================
 -- Create Dimension: gold.dim_products
