9ba6b5a3dd097622cb64647444f8518f34fdb2fb
Gisela Campagno <giselacampagno@gmail.com>
2025-02-25 19:51:43 -0300

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -15,37 +15,37 @@ Usage Notes:
 */
 
 -- ====================================================================
--- Checking 'gold.dim_customers'
+-- Checking 'gold2.dim_customers'
 -- ====================================================================
 -- Check for Uniqueness of Customer Key in gold.dim_customers
 -- Expectation: No results 
 SELECT 
     customer_key,
     COUNT(*) AS duplicate_count
-FROM gold.dim_customers
+FROM gold2.dim_customers
 GROUP BY customer_key
 HAVING COUNT(*) > 1;
 
 -- ====================================================================
--- Checking 'gold.product_key'
+-- Checking 'gold2.product_key'
 -- ====================================================================
 -- Check for Uniqueness of Product Key in gold.dim_products
 -- Expectation: No results 
 SELECT 
     product_key,
     COUNT(*) AS duplicate_count
-FROM gold.dim_products
+FROM gold2.dim_products
 GROUP BY product_key
 HAVING COUNT(*) > 1;
 
 -- ====================================================================
--- Checking 'gold.fact_sales'
+-- Checking 'gold2.fact_sales'
 -- ====================================================================
 -- Check the data model connectivity between fact and dimensions
 SELECT * 
-FROM gold.fact_sales f
-LEFT JOIN gold.dim_customers c
+FROM gold2.fact_sales f
+LEFT JOIN gold2.dim_customers c
 ON c.customer_key = f.customer_key
-LEFT JOIN gold.dim_products p
+LEFT JOIN gold2.dim_products p
 ON p.product_key = f.product_key
 WHERE p.product_key IS NULL OR c.customer_key IS NULL;
